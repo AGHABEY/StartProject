a7113c248710d3a969ee9a29c50b339807b0c8b7
AGHABEY <rafet.agabeyli98@gmail.com>
2023-12-17 01:23:51 +0400

    Initial commit

---
+++ FirstApp.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+const secondinHour = 3600
+
+func main() {
+	fmt.Println("Salam Qaqa")
+	distance := 60.8
+
+	fmt.Printf("Qaqa sen ol yaziram GO-da %f \n", distance*0.62137)
+}
